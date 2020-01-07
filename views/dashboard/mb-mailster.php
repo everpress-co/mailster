fe792a8dfba87dc1b5c318580ffd4638d0653bf4
Xaver <bitbucket@revaxarts.com>
2020-01-07 10:45:24 +0100

    added utm stuff

--- views/dashboard/mb-mailster.php
+++ views/dashboard/mb-mailster.php
@@ -22,7 +22,7 @@ if ( mailster()->is_verified() ) {
 		<a href="https://mailster.co/manage-licenses/" class="external"><?php esc_html_e( 'Manage Licenses', 'mailster' ); ?></a> |
 		<a href="<?php echo admin_url( 'admin.php?page=mailster_dashboard&reset_license=' . wp_create_nonce( 'mailster_reset_license' ) ); ?>" class="reset-license"><?php esc_html_e( 'Reset License', 'mailster' ); ?></a> |
 		<?php endif; ?>
-		<a href="https://mailster.co/go/buy/?utm_campaign=plugin&utm_medium=dashboard" class="external"><?php esc_html_e( 'Buy new License', 'mailster' ); ?></a>
+		<a href="https://mailster.co/go/buy/?utm_campaign=plugin&utm_medium=dashboard&utm_source=mailster_plugin" class="external"><?php esc_html_e( 'Buy new License', 'mailster' ); ?></a>
 	</dd>
 </dl>
 <dl class="mailster-icon mailster-icon-delete not-valid">
@@ -32,18 +32,20 @@ if ( mailster()->is_verified() ) {
 		<?php if ( current_user_can( 'mailster_manage_licenses' ) ) : ?>
 		<a href="https://mailster.co/manage-licenses/" class="external"><?php esc_html_e( 'Manage Licenses', 'mailster' ); ?></a> |
 		<?php endif; ?>
-		<a href="https://mailster.co/go/buy/?utm_campaign=plugin&utm_medium=dashboard" class="external"><?php esc_html_e( 'Buy new License', 'mailster' ); ?></a>
+		<a href="https://mailster.co/go/buy/?utm_campaign=plugin&utm_medium=dashboard&utm_source=mailster_plugin" class="external"><?php esc_html_e( 'Buy new License', 'mailster' ); ?></a>
 	</dd>
 </dl>
 <dl class="mailster-icon mailster-icon-reload update-not-available">
 	<dt><?php printf( esc_html__( 'Installed Version %s', 'mailster' ), MAILSTER_VERSION ); ?></dt>
-		<dd><?php esc_html_e( 'You have the latest version', 'mailster' ); ?></dd>
-		<dd><span class="lighter"><?php echo isset( $plugin_info->last_update ) ? sprintf( esc_html__( 'checked %s ago', 'mailster' ), '<span class="update-last-check">' . human_time_diff( $plugin_info->last_update ) . '</span>' ) . ' &ndash; ' : ''; ?></span> <span class="lighter"><a href="" class="check-for-update"><?php esc_html_e( 'Check Again', 'mailster' ); ?></a></span></dd>
+	<dd><?php esc_html_e( 'You have the latest version', 'mailster' ); ?></dd>
+	<dd><span class="lighter"><?php echo isset( $plugin_info->last_update ) ? sprintf( esc_html__( 'checked %s ago', 'mailster' ), '<span class="update-last-check">' . human_time_diff( $plugin_info->last_update ) . '</span>' ) . ' &ndash; ' : ''; ?></span> <span class="lighter"><a href="" class="check-for-update"><?php esc_html_e( 'Check Again', 'mailster' ); ?></a></span>
+	</dd>
 </dl>
 <dl class="mailster-icon mailster-icon-reload update-available">
 	<dt><?php printf( esc_html__( 'Installed Version %s', 'mailster' ), MAILSTER_VERSION ); ?></dt>
-		<dd><?php esc_html_e( 'A new Version is available', 'mailster' ); ?></dd>
-		<dd><a class="thickbox" href="<?php echo network_admin_url( 'plugin-install.php?tab=plugin-information&amp;plugin=mailster&amp;section=changelog&amp;TB_iframe=true&amp;width=772&amp;height=745' ); ?>"><?php esc_html_e( 'view changelog', 'mailster' ); ?></a> <?php esc_html_e( 'or', 'mailster' ); ?> <a href="update.php?action=upgrade-plugin&plugin=<?php echo urlencode( MAILSTER_SLUG ); ?>&_wpnonce=<?php echo wp_create_nonce( 'upgrade-plugin_' . MAILSTER_SLUG ); ?>" class="update-button"><?php printf( esc_html__( 'update to %s now', 'mailster' ), '<span class="update-version">' . $plugin_info->new_version . '</span>' ); ?></a></dd>
+	<dd><?php esc_html_e( 'A new Version is available', 'mailster' ); ?></dd>
+	<dd><a class="thickbox" href="<?php echo network_admin_url( 'plugin-install.php?tab=plugin-information&amp;plugin=mailster&amp;section=changelog&amp;TB_iframe=true&amp;width=772&amp;height=745' ); ?>"><?php esc_html_e( 'view changelog', 'mailster' ); ?></a> <?php esc_html_e( 'or', 'mailster' ); ?> <a href="update.php?action=upgrade-plugin&plugin=<?php echo urlencode( MAILSTER_SLUG ); ?>&_wpnonce=<?php echo wp_create_nonce( 'upgrade-plugin_' . MAILSTER_SLUG ); ?>" class="update-button"><?php printf( esc_html__( 'update to %s now', 'mailster' ), '<span class="update-version">' . $plugin_info->new_version . '</span>' ); ?></a>
+	</dd>
 </dl>
 <dl class="mailster-icon mailster-icon-support">
 	<dt><?php esc_html_e( 'Support', 'mailster' ); ?></dt>
@@ -59,12 +61,12 @@ if ( mailster()->is_verified() ) {
 	<dt><?php esc_html_e( 'Translation', 'mailster' ); ?> </dt>
 	<?php if ( mailster( 'translations' )->translation_installed() ) : ?>
 		<?php $name = ( esc_html_x( 'Thanks for using Mailster in %s!', 'Your language', 'mailster' ) == 'Thanks for using Mailster in %s!' ) ? $set->name : $set->native_name; ?>
-	<dd><?php printf( esc_html_x( 'Thanks for using Mailster in %s!', 'Your language', 'mailster' ), '<strong>' . $name . '</strong>' ); ?></dd>
+	<dd><?php printf( esc_html_x( 'Thanks for using Mailster in %s!', 'Your language', 'mailster' ), '<strong>' . esc_html( $name ) . '</strong>' ); ?></dd>
 		<?php if ( mailster( 'translations' )->translation_available() ) : ?>
 	<dd><a href="" class="load-language"><strong><?php esc_html_e( 'Update Translation', 'mailster' ); ?></strong></a></dd>
 		<?php endif; ?>
 	<?php elseif ( mailster( 'translations' )->translation_available() ) : ?>
-	<dd><?php printf( esc_html__( 'Mailster is available in %s!', 'mailster' ), '<strong>' . $set->name . '</strong>' ); ?></dd>
+	<dd><?php printf( esc_html__( 'Mailster is available in %s!', 'mailster' ), '<strong>' . esc_html( $set->name ) . '</strong>' ); ?></dd>
 	<dd><a href="" class="load-language"><strong><?php esc_html_e( 'Download Translation', 'mailster' ); ?></strong></a></dd>
 	<?php endif; ?>
 	<dd><span class="lighter"><?php printf( esc_html__( 'Currently %s translated.', 'mailster' ), '<strong>' . $set->percent_translated . '%</strong>' ); ?></span></dd>
