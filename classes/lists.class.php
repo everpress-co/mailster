b0ecb8bc7b86c8436d7bca35c304da673b41a7af
Xaver <bitbucket@revaxarts.com>
2020-04-14 09:31:36 +0200

    * fixed: redirection error on confirmation

--- classes/lists.class.php
+++ classes/lists.class.php
@@ -478,6 +478,7 @@ class MailsterLists {
 				$ids = array( (int) $ids );
 			}
 
+			$ids = array_filter( $ids, 'is_numeric' );
 			if ( empty( $ids ) ) {
 				return true;
 			}
@@ -487,6 +488,7 @@ class MailsterLists {
 				$subscriber_ids = array( (int) $subscriber_ids );
 			}
 
+			$subscriber_ids = array_filter( $subscriber_ids, 'is_numeric' );
 			if ( empty( $subscriber_ids ) ) {
 				return true;
 			}
