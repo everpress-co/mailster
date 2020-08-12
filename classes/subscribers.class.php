a50f0f3a5ccc5691925f9f11700cc827ec73078c
Xaver <xaver@everpress.co>
2020-08-12 15:17:35 +0200

    more sanitisation

--- classes/subscribers.class.php
+++ classes/subscribers.class.php
@@ -967,11 +967,12 @@ class MailsterSubscribers {
 
 		$now = time();
 
-		$data         = array();
-		$meta         = array();
-		$customfields = array();
-		$lists        = null;
-		$meta_keys    = $this->get_meta_keys( true );
+		$data          = array();
+		$meta          = array();
+		$customfields  = array();
+		$lists         = null;
+		$subscriber_id = null;
+		$meta_keys     = $this->get_meta_keys( true );
 
 		$entry = $this->verify( $entry );
 		if ( is_wp_error( $entry ) ) {
@@ -985,9 +986,12 @@ class MailsterSubscribers {
 			unset( $entry['_lists'] );
 		}
 
-		if ( isset( $entry['ID'] ) && empty( $entry['ID'] ) ) {
-			$subscriber_id = (int) $entry['ID'];
-			unset( $entry['ID'] );
+		if ( isset( $entry['ID'] ) ) {
+			if ( ! empty( $entry['ID'] ) ) {
+				$subscriber_id = (int) $entry['ID'];
+			} else {
+				unset( $entry['ID'] );
+			}
 		}
 
 		foreach ( $entry as $key => $value ) {
