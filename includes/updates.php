90138b3b31a031e54ad800e55bbc7787a97961a8
Xaver <xaver@everpress.co>
2021-02-22 15:37:33 +0100

    moved auto_send_at_once option

--- includes/updates.php
+++ includes/updates.php
@@ -598,11 +598,10 @@ if ( $old_version ) {
 				mailster( 'geo' )->update();
 			}
 
-			$mailster_options['auto_send_at_once'] = true;
-
 		case '2.4.17':
 			// move to "default" or after latests release versions
 			$mailster_options['db_update_required'] = true;
+			$mailster_options['auto_send_at_once'] = false;
 
 		default:
 			// reset translations
