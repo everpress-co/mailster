920387fefe8f56ba0bd6fb9ab678e4a74fcb07ef
Xaver <xaver@everpress.co>
2021-10-08 11:50:24 +0200

    update

--- blocks/input/edit.js
+++ blocks/input/edit.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -11,7 +11,22 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InspectorControls,
+	RichText,
+	PlainText,
+} from "@wordpress/block-editor";
+import {
+	Panel,
+	PanelBody,
+	PanelRow,
+	CheckboxControl,
+	TextControl,
+} from "@wordpress/components";
+import { Fragment, useState } from "@wordpress/element";
+
+import { more } from "@wordpress/icons";
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -19,7 +34,7 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
+import "./editor.scss";
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -29,10 +44,52 @@ import './editor.scss';
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit() {
+
+const MyCheckboxControl = () => {
+	const [isChecked, setChecked] = useState(true);
 	return (
-		<p {...useBlockProps()}>
-			{__('Mailster – hello from the editor! input', 'mailster')}
-		</p>
+		<CheckboxControl
+			label="Is author"
+			help="Is the user a author or not?"
+			checked={isChecked}
+			onChange={setChecked}
+		/>
+	);
+};
+
+export default function Edit(props) {
+	const { attributes, setAttributes, isSelected } = props;
+	return (
+		<Fragment>
+			<div {...useBlockProps()}>
+				<RichText
+					tagName="label"
+					value={attributes.label}
+					onChange={(val) => setAttributes({ label: val })}
+					placeholder={isSelected && __("Enter Label", "mailster")}
+				/>
+				<TextControl value="" disabled="{true}" />
+				{isSelected &&
+					__("Mailster – hello from the editor! input", "mailster")}
+			</div>
+			<InspectorControls>
+				<Panel>
+					<PanelBody title="My Block Settings" initialOpen={true}>
+						<PanelRow>
+							<MyCheckboxControl />
+						</PanelRow>
+						<PanelRow>
+							<TextControl
+								label="Label"
+								value={attributes.label}
+								onChange={(val) =>
+									setAttributes({ label: val })
+								}
+							/>
+						</PanelRow>
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+		</Fragment>
 	);
 }
