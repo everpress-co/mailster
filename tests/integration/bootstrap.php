01c46741e1cc2025ae8a057854c0732231ff2f94
Xaver <xaver@everpress.co>
2022-07-01 15:38:11 +0200

    added setup

--- tests/integration/bootstrap.php
+++ tests/integration/bootstrap.php
@@ -46,6 +46,56 @@ function _plugins_url( $url, $path, $plugin ) {
 
 function _setup() {
 
+	global $wpdb;
+
+	if ( ! $wpdb->has_connected ) {
+		die( 'no database' );
+	}
+
+	global $current_user;
+	$current_user = new WP_User( 1 );
+	$current_user->set_role( 'administrator' );
+	wp_update_user(
+		array(
+			'ID'         => 1,
+			'first_name' => 'Admin',
+			'last_name'  => 'User',
+		)
+	);
+
+	global $wp_rewrite, $wp_query;
+	$GLOBALS['wp_rewrite']->init();
+	$GLOBALS['wp_rewrite']->set_permalink_structure( '/%postname%/' );
+	flush_rewrite_rules( false );
+
+	activate_plugin( 'mailster/mailster.php' );
+	mailster()->activate();
+
+	mailster_update_option(
+		array(
+
+			'system_mail'             => 1,
+
+			'subscriber_notification' => false,
+			'homepage'                => 3,
+			'antiflood'               => 0,
+
+			// 'deliverymethod'          => 'smtp',
+
+			// 'smtp_host'               => '127.0.0.1',
+			// 'smtp_port'               => 1025,
+			// 'smtp_timeout'            => 10,
+			// 'smtp_secure'             => false,
+			// 'smtp_auth'               => false,
+			// 'smtp_user'               => '',
+			// 'smtp_pwd'                => '',
+
+			'remove_data'             => true,
+			'disable_cache'           => true,
+
+		)
+	);	
+
 	// run this code once
 }
 
