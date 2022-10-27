6698dba04fabd1878e5a8d790d622cea74bf73e2
Xaver <881729+evrpress@users.noreply.github.com>
2022-10-27 13:05:13 +0200

    add list unsubscribe header to notifications

--- classes/notification.class.php
+++ classes/notification.class.php
@@ -457,6 +457,21 @@ class MailsterNotification {
 					'hash'         => $subscriber->hash,
 				)
 			);
+
+			// add list unsubscribe headers
+			$listunsubscribe = array();
+			if ( mailster_option( 'mail_opt_out' ) ) {
+				$listunsubscribe_mail    = $this->mail->bouncemail ? $this->mail->bouncemail : $this->mail->from;
+				$listunsubscribe_subject = rawurlencode( 'Please remove me from the list' );
+				$listunsubscribe_link    = mailster()->get_unsubscribe_link( null, $subscriber->hash );
+				$listunsubscribe_body    = rawurlencode( "Please remove me from your list! {$subscriber->email} X-Mailster: {$subscriber->hash} X-Mailster-ID: {$MID} Link: {$listunsubscribe_link}" );
+
+				$listunsubscribe[] = "<mailto:$listunsubscribe_mail?subject=$listunsubscribe_subject&body=$listunsubscribe_body>";
+			}
+			$listunsubscribe[] = '<' . mailster( 'frontpage' )->get_link( 'unsubscribe', $subscriber->hash ) . '>';
+
+			$this->mail->add_header( 'List-Unsubscribe', implode( ',', $listunsubscribe ) );
+
 		}
 
 		$placeholder->add(
