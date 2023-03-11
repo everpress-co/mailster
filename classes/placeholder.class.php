1c10e357c6c95f01ac345137a19c559ed1ffbe20
Xaver <881729+evrpress@users.noreply.github.com>
2023-03-11 11:44:53 +0100

    fixed: PHP throws error if str_replace with a value below 0

--- classes/placeholder.class.php
+++ classes/placeholder.class.php
@@ -1105,7 +1105,11 @@ class MailsterPlaceholder {
 						$preview_text_fix = apply_filters( 'mailster_preview_text_fix', true );
 
 						if ( $preview_text_fix ) {
-							$replace .= str_repeat( ' &#847;', 300 - strlen( $replace ) );
+							$count = 300 - strlen( $replace );
+							// PHP throws an error if $count = 0, preventing the campaign from being sent. This check patch this.
+							if ( $count > 0 ) {
+								$replace .= str_repeat( ' &#847;', $count );
+							}
 						}
 					}
 
