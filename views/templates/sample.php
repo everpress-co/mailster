3dd0f8cfd521c321733249f39694887b80914d04
Xaver <xaver@everpress.co>
2023-11-08 15:03:49 +0100

    updated setup

--- views/templates/sample.php
+++ views/templates/sample.php
@@ -25,6 +25,8 @@ if ( $slug !== 'mailster' ) {
 ?>
 <div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>" tabindex="0" data-slug="<?php echo esc_attr( $slug ); ?>">
 
+	
+
 	<?php
 
 	$html    = base64_decode( $item['sample'] );
@@ -47,34 +49,16 @@ if ( $slug !== 'mailster' ) {
 	?>
 
 	<div class="mailster-template-preview">
+		<header>
+			<?php echo esc_html( $item['name'] ); ?>
+			<span class="theme-badge theme-default-badge"><?php esc_html_e( 'Current', 'mailster' ); ?></span>
+		</header>
 		<div class="mailster-template-preview-browser">
 		<iframe src="data:text/html;base64,<?php echo base64_encode( $content ); ?> " height="100%" width="100%"></iframe>
 		</div>
 
 	<?php if ( ! $item['is_supported'] && ! $item['installed'] ) : ?>
 	<div class="notice inline update-message notice-error notice-alt"><p><?php printf( esc_html__( 'This template requires Mailster version %s or above. Please update first.', 'mailster' ), '<strong>' . $item['requires'] . '</strong>' ); ?></p></div>
-	<?php endif; ?>
-	<?php if ( $item['update_available'] ) : ?>
-	<div class="notice inline update-message notice-warning notice-alt theme-has-update">
-		<p><?php esc_html_e( 'New version available.', 'mailster' ); ?>
-		<?php if ( $item['download_url'] ) : ?>
-		<a class="button-link update" data-width="800" data-height="80%" href="<?php echo esc_url( $item['download_url'] ); ?>"><?php esc_html_e( 'Update now', 'mailster' ); ?></a>
-		<?php elseif ( $item['download'] ) : ?>
-			<?php
-			$url = add_query_arg(
-				array(
-					'action'   => 'mailster_template_endpoint',
-					'slug'     => $slug,
-					'url'      => rawurlencode( $item['download'] ),
-					'_wpnonce' => wp_create_nonce( 'mailster_download_template_' . $slug ),
-				),
-				admin_url( 'admin-ajax.php' )
-			);
-			?>
-		<a class="button-link request-update popup" data-width="800" data-height="80%" href="<?php echo esc_url( $url ); ?>"><?php esc_html_e( 'Update now', 'mailster' ); ?></a>
-		<?php endif; ?>
-		</p>
-	</div>
 	<?php endif; ?>
 	<span class="more-details"><?php esc_html_e( 'Details & Preview', 'mailster' ); ?></span>
 	<div class="theme-author"><?php printf( esc_html__( 'By %s', 'mailster' ), $item['author'] ); ?></div>
