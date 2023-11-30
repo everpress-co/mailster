c5e118a68f78b4570865a02fdd75fa3ea1b09ed3
Xaver <xaver@everpress.co>
2023-11-30 11:56:14 +0100

    better form preview

--- assets/js/block-form-overview-script.js
+++ assets/js/block-form-overview-script.js
@@ -3,7 +3,32 @@ mailster = (function (mailster, $, window, document) {
 
 	mailster.$.document.on('click', '.form_preview a', function () {
 		var url = $(this).attr('href');
-		tb_show('', url + '?KeepThis=true&TB_iframe=true&height=600&width=800');
+		var w = mailster.$.window.width() - 32;
+		var h = mailster.$.window.height();
+		var overlay = $('#form-preview-overlay');
+
+		if (!overlay.length) {
+			overlay = $(
+				'<div id="form-preview-overlay"><iframe allowtransparency="true"></iframe></div>'
+			).appendTo('body');
+		}
+
+		var iframe = overlay.find('iframe');
+		iframe.attr('src', url);
+
+		var title = $(this).data('title');
+
+		//show thickbox with id form-preview-overlay
+		mailster.util.tb_show(
+			title,
+			'#TB_inline?x=1' +
+				'&width=' +
+				w +
+				'&height=' +
+				h +
+				'&inlineId=form-preview-overlay',
+			null
+		);
 		return false;
 	});
 
