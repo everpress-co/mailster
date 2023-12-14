6478e716fbc02efd51fd918370795a12fb624b21
Xaver <xaver@everpress.co>
2023-12-14 14:11:13 +0100

    improved: better handling of offset on time based automations

--- blocks/workflows/trigger/DateSelector.js
+++ blocks/workflows/trigger/DateSelector.js
@@ -11,36 +11,18 @@ import { __, sprintf } from '@wordpress/i18n';
 import {
 	Button,
 	PanelRow,
-	DropdownMenu,
-	TextControl,
-	MenuGroup,
 	__experimentalNumberControl as NumberControl,
-	Spinner,
-	PanelBody,
-	__experimentalItemGroup as ItemGroup,
 	BaseControl,
-	Card,
-	Flex,
-	FlexBlock,
-	FlexItem,
-	Tip,
 	DateTimePicker,
 	SelectControl,
 	Popover,
-	DatePicker,
 	TimePicker,
+	FlexBlock,
 } from '@wordpress/components';
 
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { useSelect } from '@wordpress/data';
-import { useEntityProp } from '@wordpress/core-data';
 import { useEffect, useState } from '@wordpress/element';
 
-import * as Icons from '@wordpress/icons';
-import {
-	useBlockProps,
-	__experimentalLinkControl as LinkControl,
-} from '@wordpress/block-editor';
 import { dateI18n, gmdateI18n, humanTimeDiff } from '@wordpress/date';
 
 /**
@@ -52,13 +34,19 @@ import {
 	TIME_FORMAT,
 	DATE_FORMAT,
 } from './constants';
+import { set } from 'lodash';
+import { Flex } from '@wordpress/components';
+import { FlexItem } from '@wordpress/components';
 
 export default function Selector(props) {
 	const { attributes, setAttributes, isAnniversary = false } = props;
-	const { date, field, offset } = attributes;
+	const { date, field, offset = 0 } = attributes;
 
 	const site = useSelect((select) => select('core').getSite());
 	const [popover, setPopover] = useState(false);
+	const [relative, setRelative] = useState('');
+	const [amount, setAmount] = useState(0);
+	const [unit, setUnit] = useState(0);
 
 	const setDate = (newDate) => {
 		// store in UTC
@@ -73,7 +61,35 @@ export default function Selector(props) {
 		return field.type == 'date';
 	});
 
-	const isInPast = +new Date() - +new Date(date) > 0;
+	useEffect(() => {
+		if (!offset) return;
+		if (offset % 604800 == 0) {
+			setUnit(604800);
+			setAmount(offset / 604800);
+		} else if (offset % 86400 == 0) {
+			setUnit(86400);
+			setAmount(offset / 86400);
+		} else if (offset % 3600 == 0) {
+			setUnit(3600);
+			setAmount(offset / 3600);
+		} else {
+			setUnit(60);
+			setAmount(Math.floor(offset / 60));
+		}
+	}, [offset]);
+	useEffect(() => {
+		if (field) return;
+
+		setUnit(0);
+		setAmount(0);
+	}, [field]);
+
+	useEffect(() => {
+		const newOffset = amount * unit;
+		setAttributes({ offset: newOffset || undefined });
+	}, [unit, amount]);
+
+	const isInPast = +new Date() - +new Date(date) - offset * 1000 > 0;
 
 	return (
 		<>
@@ -167,15 +183,6 @@ export default function Selector(props) {
 										</Popover>
 									)}
 								</Button>
-								<NumberControl
-									label={__('Offset', 'mailster')}
-									labelPosition="side"
-									value={offset}
-									onChange={(val) => {
-										setAttributes({ offset: parseInt(val, 10) });
-									}}
-									step={1}
-								/>
 							</>
 						)}
 					</BaseControl>
@@ -247,20 +254,60 @@ export default function Selector(props) {
 										</Popover>
 									)}
 								</Button>
-								<NumberControl
-									label={__('Offset', 'mailster')}
-									labelPosition="side"
-									value={offset}
-									onChange={(val) => {
-										setAttributes({ offset: parseInt(val, 10) });
-									}}
-									step={1}
-								/>
 							</>
 						)}
 					</BaseControl>
 				)}
 			</PanelRow>
+			{field && (
+				<PanelRow>
+					<BaseControl
+						label={__('Offset', 'mailster')}
+						help={__(
+							'Offset the time to trigger either before ar after the defined date.',
+							'mailster'
+						)}
+					>
+						<Flex expanded={false}>
+							<FlexBlock>
+								<NumberControl
+									step={1}
+									shiftStep={10}
+									value={amount}
+									onChange={(val) => {
+										setAmount(val);
+									}}
+								/>
+							</FlexBlock>
+							<FlexBlock>
+								<SelectControl
+									value={unit}
+									onChange={(val) => {
+										setUnit(val);
+									}}
+									options={[
+										{ value: 1, label: __('Seconds', 'mailster') },
+										{ value: 60, label: __('Minutes', 'mailster') },
+										{ value: 3600, label: __('Hours', 'mailster') },
+										{ value: 86400, label: __('Days', 'mailster') },
+										{ value: 604800, label: __('Weeks', 'mailster') },
+									]}
+								/>
+							</FlexBlock>
+							<FlexBlock>
+								<Button
+									variant="link"
+									size={'compact'}
+									isDestructive
+									onClick={() => setAmount(0)}
+								>
+									{__('Reset', 'mailster')}
+								</Button>
+							</FlexBlock>
+						</Flex>
+					</BaseControl>
+				</PanelRow>
+			)}
 			{!isAnniversary && !field && isInPast && (
 				<PanelRow>
 					<Button
